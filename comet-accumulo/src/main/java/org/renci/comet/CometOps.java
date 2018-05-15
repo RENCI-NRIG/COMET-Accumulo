cc1d78d250757037a0fa8621c18ada7b110cf6f6
Cong Wang <congwang@rciadmins-mbp-2.wireless-1x.unc.edu>
2018-05-15 14:43:29 -0400

    check if contextID is strong enough

--- comet-accumulo/src/main/java/org/renci/comet/CometOps.java
+++ comet-accumulo/src/main/java/org/renci/comet/CometOps.java
@@ -50,6 +50,33 @@ public class CometOps implements CometOpsIfce {
     //public String tableName="accu-test";
     public String tableName="trace";
 
+	/**
+	 * Check if password contains:
+	 * At least 8 chars
+	 * Contains at least one digit
+	 * Contains at least one lower alpha char and one upper alpha char
+	 * Contains at least one char within a set of special chars (@#%$^ etc.)
+	 * Does not contain space, tab, etc.
+	 *
+	 * ^                 # start-of-string
+	 * (?=.*[0-9])       # a digit must occur at least once
+	 * (?=.*[a-z])       # a lower case letter must occur at least once
+	 * (?=.*[A-Z])       # an upper case letter must occur at least once
+	 * (?=.*[@#$%^&+=])  # a special character must occur at least once
+	 * (?=\S+$)          # no whitespace allowed in the entire string
+	 * .{8,}             # anything, at least eight places though
+	 * $                 # end-of-string
+	 *
+	 * @param expression
+	 * @return
+	 */
+	public static boolean isStrong(String expression) {
+		if (expression != null) {
+			return expression.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
+		}
+		return false;
+	}
+
 	public static byte[] serialize(Object obj) throws IOException {
 	    ByteArrayOutputStream out = new ByteArrayOutputStream();
 	    ObjectOutputStream os = new ObjectOutputStream(out);
