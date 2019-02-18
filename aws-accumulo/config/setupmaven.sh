34c3aa1f902c51f7340501d24c035101a5d819a8
Komal Thareja <kthar10@renci.org>
2019-02-18 11:23:47 -0500

    update version

--- aws-accumulo/config/setupmaven.sh
+++ aws-accumulo/config/setupmaven.sh
@@ -9,7 +9,7 @@ HOME=/root
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 
-wget http://mirror.stjschools.org/public/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+wget http://apache.cs.utah.edu/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
 tar -zxvf apache-maven-$MAVEN_VERSION-bin.tar.gz -C /opt
 
 #Setup maven profile
